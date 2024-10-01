f2207d0539ce81fcfc27ecd6d2cd498bd039b391
Emerson Estrella <emerson.estrella@gmail.com>
2024-10-01 21:02:08 +0200

    feat: Website v1.0.1

---
+++ theme.485d509c.js
@@ -0,0 +1,2 @@
+(0,globalThis.parcelRequire10c2.register)("iO6Mx",function(e,t){function i(){document.body.classList.add("light")}Object.defineProperty(e.exports,"setTheme",{get:()=>i,set:void 0,enumerable:!0,configurable:!0})});
+//# sourceMappingURL=theme.485d509c.js.map
