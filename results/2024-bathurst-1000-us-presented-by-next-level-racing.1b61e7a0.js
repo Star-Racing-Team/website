9a549f5a2c95cd86e5420dc289f380c3f0af5813
Emerson Estrella <emerson.estrella@gmail.com>
2024-10-06 23:54:25 +0200

    feat: Website v1.0.18

---
+++ results/2024-bathurst-1000-us-presented-by-next-level-racing.1b61e7a0.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2024-bathurst-1000-us-presented-by-next-level-racing.1b61e7a0.js.map
---
+++ standings/2024-bathurst-1000-us-presented-by-next-level-racing.431a7cf7.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2024-bathurst-1000-us-presented-by-next-level-racing.431a7cf7.js.map
