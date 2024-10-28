bb4869c4e58834da480b54df92722b50163d2fc2
Emerson Estrella <emerson.estrella@gmail.com>
2024-10-28 10:29:02 +0100

    feat: Website v1.0.32

---
+++ series/prl-toyota-gr86-cup-2024-season-4.a5db7245.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=prl-toyota-gr86-cup-2024-season-4.a5db7245.js.map
