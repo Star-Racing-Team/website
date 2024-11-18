bf18552b7d0944a81ba6a91211e9a9d52ae4aac0
Emerson Estrella <emerson.estrella@gmail.com>
2024-11-18 19:10:40 +0100

    feat: Website v1.0.35

---
+++ standings/fuji-8-hour.0dd4dbbf.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=fuji-8-hour.0dd4dbbf.js.map
