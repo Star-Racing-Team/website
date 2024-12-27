d9fdc3cd0b787cb9ff7822377e473660b353e0eb
Emerson Estrella <emerson.estrella@gmail.com>
2024-12-27 22:39:30 +0100

    feat: Website v1.0.40

---
+++ results/imsa-endurance-series-2025-season-1.415997b3.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=imsa-endurance-series-2025-season-1.415997b3.js.map
