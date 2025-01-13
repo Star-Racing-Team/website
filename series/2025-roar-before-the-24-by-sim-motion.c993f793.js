5b4c104944078a2a449b0e43a2e74fd7dc6c5677
Emerson Estrella <emerson.estrella@gmail.com>
2025-01-13 23:57:43 +0100

    feat: Website v1.0.55

---
+++ series/2025-roar-before-the-24-by-sim-motion.c993f793.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-roar-before-the-24-by-sim-motion.c993f793.js.map
