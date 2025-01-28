00c6afafdb07f774ce43ba745230a7d830dd9538
Emerson Estrella <emerson.estrella@gmail.com>
2025-01-28 14:45:19 +0100

    feat: Website v1.0.62

---
+++ series/racing-prodigy-bmw-m2-cs-racing-cup-2025-season-1.1b20d994.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=racing-prodigy-bmw-m2-cs-racing-cup-2025-season-1.1b20d994.js.map
