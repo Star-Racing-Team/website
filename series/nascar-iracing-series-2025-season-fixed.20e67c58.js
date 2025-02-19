aff773b70283a8d2cb7e00fe4346f514f56083cb
Emerson Estrella <emerson.estrella@gmail.com>
2025-02-19 17:20:01 +0100

    feat: Website v1.0.67

---
+++ series/nascar-iracing-series-2025-season-fixed.20e67c58.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=nascar-iracing-series-2025-season-fixed.20e67c58.js.map
