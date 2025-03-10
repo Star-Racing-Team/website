e244b78afd9501c3d4b8ded5fd88737e03229138
Emerson Estrella <emerson.estrella@gmail.com>
2025-03-10 09:22:34 +0100

    feat: Website v1.0.75

---
+++ series/itested-series-2025-season-1-fixed.b00b6cfc.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=itested-series-2025-season-1-fixed.b00b6cfc.js.map
