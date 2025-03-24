36cd0b5ee4dcfef85231756c5624a8ee12287001
Emerson Estrella <emerson.estrella@gmail.com>
2025-03-24 15:11:31 +0100

    feat: Website v1.0.76

---
+++ series/pro-4-off-road-racing-series-fixed-2025-season-2.d20ab429.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=pro-4-off-road-racing-series-fixed-2025-season-2.d20ab429.js.map
