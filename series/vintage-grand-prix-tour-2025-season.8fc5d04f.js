af982348c7970607d4767237339954c6dce14dbf
Emerson Estrella <emerson.estrella@gmail.com>
2025-04-23 17:36:56 +0200

    feat: Website v1.0.79

---
+++ series/vintage-grand-prix-tour-2025-season.8fc5d04f.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=vintage-grand-prix-tour-2025-season.8fc5d04f.js.map
