91583b8b86a2be0a76eed11bce4bb3c1af988fc0
Emerson Estrella <emerson.estrella@gmail.com>
2025-04-27 23:19:32 +0200

    feat: Website v1.0.80

---
+++ results/2025-road-america-500.aa6ec5d6.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-road-america-500.aa6ec5d6.js.map
---
+++ standings/2025-road-america-500.f64682ff.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-road-america-500.f64682ff.js.map
