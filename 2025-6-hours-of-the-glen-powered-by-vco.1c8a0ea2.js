2f24731a7ec4d0c35392010822a5594e92edc46b
Emerson Estrella <emerson.estrella@gmail.com>
2025-06-30 22:20:56 +0200

    feat: Website v1.0.89

---
+++ 2025-6-hours-of-the-glen-powered-by-vco.1c8a0ea2.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-6-hours-of-the-glen-powered-by-vco.1c8a0ea2.js.map
