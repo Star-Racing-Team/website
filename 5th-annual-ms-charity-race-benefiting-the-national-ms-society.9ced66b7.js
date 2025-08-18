9a1dc8dae2d949dabe0d67fedf0e7c7dc26934e8
Emerson Estrella <emerson.estrella@gmail.com>
2025-08-18 14:52:45 +0200

    feat: Website v1.0.95

---
+++ 5th-annual-ms-charity-race-benefiting-the-national-ms-society.9ced66b7.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=5th-annual-ms-charity-race-benefiting-the-national-ms-society.9ced66b7.js.map
