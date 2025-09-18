fb467be18c8df95501bc0ff3ceec0b9810a6b4ef
Emerson Estrella <emerson.estrella@gmail.com>
2025-09-18 13:56:46 +0200

    feat: Website v1.0.97

---
+++ justas-simaitis.99d99895.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(3) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=justas-simaitis.99d99895.js.map
---
+++ pro-2-lite-off-road-rookie-series-by-trak-racer-2025-season-4.74d79e2a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=pro-2-lite-off-road-rookie-series-by-trak-racer-2025-season-4.74d79e2a.js.map
