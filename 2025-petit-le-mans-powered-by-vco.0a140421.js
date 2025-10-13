fdf9e03012a36ea152e7ecc9ff084efab29a5e47
Emerson Estrella <emerson.estrella@gmail.com>
2025-10-13 17:21:22 +0200

    feat: Website v1.0.101

---
+++ 2025-petit-le-mans-powered-by-vco.0a140421.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-petit-le-mans-powered-by-vco.0a140421.js.map
---
+++ 2025-petit-le-mans-powered-by-vco.88b10317.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-petit-le-mans-powered-by-vco.88b10317.js.map
