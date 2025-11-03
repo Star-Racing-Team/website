6f145aeaba542777cbc84548871ea8d498680b34
Emerson Estrella <emerson.estrella@gmail.com>
2025-11-03 19:35:21 +0100

    feat: Website v1.0.103

---
+++ 2025-brscc-ff1600-festival.364d0dad.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-brscc-ff1600-festival.364d0dad.js.map
---
+++ 2025-iracing-mx-500-fixed.caf2dde9.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-iracing-mx-500-fixed.caf2dde9.js.map
