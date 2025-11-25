9c7353a3cc2a5b260ce6b3627f28d12ffafb1001
Emerson Estrella <emerson.estrella@gmail.com>
2025-11-25 20:53:23 +0100

    feat: Website v1.0.104

---
+++ 2025-suzuka-1000km.e5bf908c.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2025-suzuka-1000km.e5bf908c.js.map
