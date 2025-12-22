937604cca02e5866accde9367d2dd2578e733824
Emerson Estrella <emerson.estrella@gmail.com>
2025-12-22 14:14:37 +0100

    feat: Website v1.0.108

---
+++ fia-cross-car-championship-2026-season-1.16cb73be.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=fia-cross-car-championship-2026-season-1.16cb73be.js.map
---
+++ gt-endurance-series-by-simucube-2026-season-1.9849d736.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=gt-endurance-series-by-simucube-2026-season-1.9849d736.js.map
