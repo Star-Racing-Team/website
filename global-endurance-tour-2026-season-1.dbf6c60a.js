f78afe68714598098159c9d90be01be44d508cec
Emerson Estrella <emerson.estrella@gmail.com>
2025-12-29 00:27:22 +0100

    feat: Website v1.0.114

---
+++ global-endurance-tour-2026-season-1.dbf6c60a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=global-endurance-tour-2026-season-1.dbf6c60a.js.map
