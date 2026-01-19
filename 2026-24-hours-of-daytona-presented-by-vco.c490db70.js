65be69c608153fb5a56bbf2a1db7f94a2556fac2
Emerson Estrella <emerson.estrella@gmail.com>
2026-01-19 13:52:58 +0100

    feat: Website v1.0.117

---
+++ 2026-24-hours-of-daytona-presented-by-vco.c490db70.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-24-hours-of-daytona-presented-by-vco.c490db70.js.map
