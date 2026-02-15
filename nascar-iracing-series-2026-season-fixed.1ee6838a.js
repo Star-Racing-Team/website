d668155ba4e68fe686541dbc272d07558950f33f
Emerson Estrella <emerson.estrella@gmail.com>
2026-02-16 00:02:12 +0100

    feat: Website v1.0.127

---
+++ nascar-iracing-series-2026-season-fixed.1ee6838a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=nascar-iracing-series-2026-season-fixed.1ee6838a.js.map
