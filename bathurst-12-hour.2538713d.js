93a890cd645c552dd5023bebb8dc11c07f950a97
Emerson Estrella <emerson.estrella@gmail.com>
2026-03-09 00:08:32 +0100

    feat: Website v1.0.141

---
+++ bathurst-12-hour.2538713d.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=bathurst-12-hour.2538713d.js.map
---
+++ bathurst-12-hour.d25f0b80.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=bathurst-12-hour.d25f0b80.js.map
