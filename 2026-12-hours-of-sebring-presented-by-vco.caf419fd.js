baca6fca178d73d5d6989aeef1c1d2665062e0ee
Emerson Estrella <emerson.estrella@gmail.com>
2026-04-20 00:23:31 +0200

    feat: Website v1.0.145

---
+++ 2026-12-hours-of-sebring-presented-by-vco.caf419fd.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-12-hours-of-sebring-presented-by-vco.caf419fd.js.map
---
+++ 2026-12-hours-of-sebring-presented-by-vco.fa96b914.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(5) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-12-hours-of-sebring-presented-by-vco.fa96b914.js.map
