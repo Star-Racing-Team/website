21ff6f4f708afc970b979607f12e038c6c0c1e6f
Emerson Estrella <emerson.estrella@gmail.com>
2026-05-04 00:19:54 +0200

    feat: Website v1.0.147

---
+++ 2026-24-hours-of-nurburgring-presented-by-thrustmaster.3cf7d241.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-24-hours-of-nurburgring-presented-by-thrustmaster.3cf7d241.js.map
