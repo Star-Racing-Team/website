ece1d563c1c7a7910900747e21a894994887dabe
Emerson Estrella <emerson.estrella@gmail.com>
2026-06-01 00:20:35 +0200

    feat: Website v1.0.150

---
+++ 2026-4-hours-at-thruxton-presented-by-thrustmaster.77b221f2.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-4-hours-at-thruxton-presented-by-thrustmaster.77b221f2.js.map
