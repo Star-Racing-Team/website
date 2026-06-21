8fceb2df1fa5ca1229242c8eead2308578d574cf
Emerson Estrella <emerson.estrella@gmail.com>
2026-06-22 00:11:47 +0200

    feat: Website v1.0.153

---
+++ rallycross-series-fixed-by-trak-racer-2026-season-3.ad9c2ee3.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=rallycross-series-fixed-by-trak-racer-2026-season-3.ad9c2ee3.js.map
