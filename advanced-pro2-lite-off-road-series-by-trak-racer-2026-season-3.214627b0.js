01ac99b210a3d5ebeecc22e0fb19520c5906c89a
Emerson Estrella <emerson.estrella@gmail.com>
2026-06-29 00:14:29 +0200

    feat: Website v1.0.154

---
+++ advanced-pro2-lite-off-road-series-by-trak-racer-2026-season-3.214627b0.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=advanced-pro2-lite-off-road-series-by-trak-racer-2026-season-3.214627b0.js.map
