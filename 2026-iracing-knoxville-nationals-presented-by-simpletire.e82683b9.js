139460b8667436fe6781c9e40e8f1d9d891f7945
Emerson Estrella <emerson.estrella@gmail.com>
2026-09-09 23:41:23 +0200

    feat: Website v1.0.178

---
+++ 2026-iracing-knoxville-nationals-presented-by-simpletire.e82683b9.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(7) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-iracing-knoxville-nationals-presented-by-simpletire.e82683b9.js.map
