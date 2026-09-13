cd6bcefdfe73074e6e6d205ef58882c6f6dba42a
Emerson Estrella <emerson.estrella@gmail.com>
2026-09-14 00:10:30 +0200

    feat: Website v1.0.179

---
+++ 2026-suzuka-1000km-presented-by-pimax.b6424d42.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".sidebar li a").forEach(e=>{e.classList.remove("bg-active","text-active-text")}),document.querySelectorAll(".sidebar li:nth-child(6) a").forEach(e=>{e.classList.add("bg-active","text-active-text")});
+//# sourceMappingURL=2026-suzuka-1000km-presented-by-pimax.b6424d42.js.map
